f9f8c5f0f59ead5371b70f3529bb42f296c4fbab
Audrey Vo <avo@hmc.edu>
2024-09-05 16:57:32 -0700

    lab 1 modified structure

--- lab1RP/source/impl_1/lab1_av.sv
+++ lab1RP/source/impl_1/lab1_av.sv
@@ -10,44 +10,10 @@ module top(
 
 
 );
-	// LED 0
-	xor g1(led[0], s[0], s[1]);
+	// call led display
+	ledDisplay s2 (s, led);
 	
-	// LED 1
-	and g2(led[1], s[2], s[3]);
-	
-	// LED 2
-	
-	logic int_osc;
-	logic pulse;
-	logic led_state = 1;
-	logic [24:0] counter = 0;
-	
-	
-	// Internal high-speed oscillator
-	HSOSC hf_osc (.CLKHFPU(1'b1), .CLKHFEN(1'b1), .CLKHF(int_osc));
-	
-	// Simple clock divider
-	always_ff @(posedge int_osc)
-		begin
-			// checks if counter has hit 10M (to get 2.4 Hz signal)
-			if(counter != 10000000) counter <= counter + 1;
-			else if (counter == 10000000 & led_state == 0) 
-				begin 
-					led_state <= 1;
-					counter <= 0;
-				end
-			else if (counter == 10000000 & led_state == 1) 
-				begin 
-					led_state <= 0;
-					counter <= 0;
-				end
-		end
-	
-	// turns led on or off
-	assign led[2] = led_state;
-	
-	
+	// call 7 segment display
 	sevenSeg s1 (s, seg);
 			
 	
---
+++ lab1RP/source/impl_1/ledDisplay.sv
@@ -0,0 +1,51 @@
+// Audrey Vo
+// avo@hmc.edu
+// 9/3/2024
+// This code controls the the 7-segement display based on the input of the switcbes.
+
+module ledDisplay (
+	input logic [3:0] s,
+	output logic [2:0] led
+);
+
+	// LED 0
+	xor g1(led[0], s[0], s[1]);
+	
+	// LED 1
+	and g2(led[1], s[2], s[3]);
+	
+	// LED 2
+	
+	logic int_osc;
+	logic pulse;
+	logic led_state = 1;
+	logic [24:0] counter = 0;
+	
+	
+	// Internal high-speed oscillator
+	HSOSC hf_osc (.CLKHFPU(1'b1), .CLKHFEN(1'b1), .CLKHF(int_osc));
+	
+	// Simple clock divider
+	always_ff @(posedge int_osc)
+		begin
+			// checks if counter has hit 10M (to get 2.4 Hz signal)
+			if(counter != 10000000) counter <= counter + 1;
+			else if (counter == 10000000 & led_state == 0) 
+				begin 
+					led_state <= 1;
+					counter <= 0;
+				end
+			else if (counter == 10000000 & led_state == 1) 
+				begin 
+					led_state <= 0;
+					counter <= 0;
+				end
+		end
+	
+	// turns led on or off
+	assign led[2] = led_state;
+	
+
+
+
+endmodule
